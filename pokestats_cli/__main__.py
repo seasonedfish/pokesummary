99e5dbe53f1747f2d1bf72879e4530a11e25c156
Fisher Sun <fisher521.fs@gmail.com>
2021-02-13 13:44:37 -0500

    Add modify.py

--- pokestats_cli/__main__.py
+++ pokestats_cli/__main__.py
@@ -5,9 +5,9 @@ import importlib.resources
 if __name__ == "__main__":
     from . import data
 
-    with importlib.resources.open_text(data, "pokemon.csv") as file:
+    with importlib.resources.open_text(data, "pokemon_modified.csv") as file:
         data_iterator = csv.reader(file)
         data = {row[2]: row[3:40]
                 for row in data_iterator}
     pp = pprint.PrettyPrinter()
-    pp.pprint(data["\"Bulbasaur\""])
+    pp.pprint(data["Bulbasaur"])
