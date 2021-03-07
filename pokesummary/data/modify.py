c4955b24e0ed0eb5b0f3989597a7c19bfa47090a
Fisher Sun <fisher521.fs@gmail.com>
2021-03-07 00:30:30 -0500

    Update modify.py

--- pokesummary/data/modify.py
+++ pokesummary/data/modify.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 """
 This module not used when the main program is executed
-because the main program reads from modified database.
+because the main program reads from modified dataset.
 """
 
 
@@ -16,20 +16,22 @@ def main():
     """
     # Open from file
     df = pd.read_csv("pokemon.csv")
-
+    # Make copy of rows that have alternate forms
     alt_pokemon = df.query("`Alternate Form Name` != ''")
-    print(alt_pokemon["Pokemon Name"].head())
 
     # Prepare regional demonyms
     regional_demonyms = [{"Alola": "Alolan"}, {"Galar": "Galarian"}]
     for dictionary in regional_demonyms:
         alt_pokemon["Alternate Form Name"].replace(dictionary, inplace=True)
-    # Append all other form names, including regional
+    # Append all form names
     alt_pokemon["Pokemon Name"] = alt_pokemon["Alternate Form Name"] + " " + alt_pokemon["Pokemon Name"]
+    # TODO: fix order of mega (e.g. Mega Charizard X, not Mega X Charizard)
 
-    # Write to file
+    # Update original dataframe
     df.update(alt_pokemon)
-    df.to_csv("new_pokemon.csv", index=False)
+    df.drop(["Alternate Form Name"], axis=1)
+    # Write to file
+    df.to_csv("pokemon_modified.csv", index=False)
 
 
 if __name__ == "__main__":
