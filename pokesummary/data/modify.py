fe69959820ca9d29ef2bc803143c509b3435e761
Fisher Sun <fisher521.fs@gmail.com>
2021-03-06 23:58:11 -0500

    Use Chiang dataset

--- pokesummary/data/modify.py
+++ pokesummary/data/modify.py
@@ -2,35 +2,34 @@
 import pandas as pd
 
 """
-This module removes the extra quotation marks from
-the original pokemon database (pokemon.csv)
-and writes to a modified database (pokemon_modified.csv).
-It is not used when the main program is executed
+This module not used when the main program is executed
 because the main program reads from modified database.
 """
 
 
 def main():
     """
-    Remove the extra quotation marks from
+    Append the alternate form names to the names themselves in
     pokemon.csv, writing to pokemon_modified.csv
 
     :return: None
     """
     # Open from file
     df = pd.read_csv("pokemon.csv")
-    # Get string columns
-    df_string_cols = df.select_dtypes(include="object")
-    # Remove leading and trailing quotation marks from string columns
-    df_string_cols = df_string_cols.apply(
-        lambda s: s.str.slice(start=1, stop=-1)
-        if s.str.startswith("\"").any() & s.str.endswith("\"").any()
-        else s
-    )
-    # Modify in place
-    df.update(df_string_cols)
+
+    pd.options.mode.use_inf_as_na = True
+    alt_pokemon = df.loc[df["Alternate Form Name"].notnull()]
+    print(alt_pokemon["Pokemon Name"].head())
+
+    # Prepare regional demonyms
+    regional_demonyms = [{"Alola": "Alolan"}, {"Galar": "Galarian"}]
+    for dictionary in regional_demonyms:
+        alt_pokemon["Alternate Form Name"].replace(dictionary)
+    # Append all other form names, including regional
+    alt_pokemon["Pokemon Name"] = alt_pokemon["Alternate Form Name"] + " " + alt_pokemon["Pokemon Name"]
+
     # Write to file
-    df.to_csv("pokemon_modified.csv", index=False)
+    df.to_csv("new_pokemon.csv")
 
 
 if __name__ == "__main__":
