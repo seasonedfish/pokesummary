8eb7ffaf3978666f45c57303ea71ef59910167aa
Fisher Sun <fisher521.fs@gmail.com>
2021-03-09 19:53:47 -0500

    Use DictReader

--- pokesummary/displaying.py
+++ pokesummary/displaying.py
@@ -1,3 +1,4 @@
 def display_summary(pokemon_stats):
     # TODO: implement
+    print(f"{pokemon_stats['Pokemon Name']}, {pokemon_stats['Classification']}")
     print(pokemon_stats)
--- pokesummary/parsing.py
+++ pokesummary/parsing.py
@@ -4,9 +4,9 @@ from importlib import resources
 
 def parse_data():
     with resources.open_text("pokesummary.data", "pokemon_modified.csv") as f:
-        data_iterator = csv.reader(f)
+        data_iterator = csv.DictReader(f)
         data_dictionary = {
-            row[2]: row[3:40]
+            row["Pokemon Name"]: row
             for row in data_iterator
         }
     return data_dictionary
