25cccaecbe9798a964b9cb23c4f98db04799b0ec
Fisher Sun <fisher521.fs@gmail.com>
2021-03-31 16:41:49 -0400

    Add docstring

--- pokesummary/parsing.py
+++ pokesummary/parsing.py
@@ -3,6 +3,16 @@ from importlib import resources
 
 
 def csv_to_2d_dict(package, csv_file, index, lambda_function=lambda x: x):
+    """
+    Parses a CSV file to a dictionary of dictionaries.
+
+    :param package: the package in which the CSV file is located
+    :param csv_file: the CSV file to parse
+    :param index: the column to use as the keys of the outer dictionary
+    :param lambda_function: an optional function to apply to every value
+    :return: A dictionary, with the keys being each row's value for the
+    index column, and the values being dictionaries of each row's other values.
+    """
     with resources.open_text(package, csv_file) as f:
         data_iterator = csv.DictReader(f)
         data_dictionary = {
