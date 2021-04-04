bc96453bec66a478309be4f41987170dd19981be
Fisher Sun <fisher521.fs@gmail.com>
2021-04-04 13:33:59 -0400

    Keep only relevant columns
    Fixes #8

--- pokesummary/data/modify_pokemon.py
+++ pokesummary/data/modify_pokemon.py
@@ -14,29 +14,45 @@ def main():
 
     :return: None
     """
-    # Open from file
     df = pd.read_csv("pokemon_original.csv")
     # Format column names
-    df.columns = df.columns\
-        .str.replace(" ", "_", regex=False)\
-        .str.replace(r"\W+", "", regex=True)\
+    df.columns = (
+        df.columns.str.replace(" ", "_", regex=False)
+        .str.replace(r"\W+", "", regex=True)
         .str.lower()
+    )
 
     # Make copy of rows that have alternate forms
     alt_pokemon = df.query("`alternate_form_name` != ''")
-
     # Prepare regional demonyms
     regional_demonyms = [{"Alola": "Alolan"}, {"Galar": "Galarian"}]
     for dictionary in regional_demonyms:
         alt_pokemon["alternate_form_name"].replace(dictionary, inplace=True)
     # Append all form names
-    alt_pokemon["pokemon_name"] = alt_pokemon["alternate_form_name"] + " " + alt_pokemon["pokemon_name"]
+    alt_pokemon["pokemon_name"] = (
+        alt_pokemon["alternate_form_name"] + " " + alt_pokemon["pokemon_name"]
+    )
     # TODO: fix order of mega (e.g. Mega Charizard X, not Mega X Charizard)
 
     # Update original dataframe
     df.update(alt_pokemon)
-    df = df.drop(["alternate_form_name"], axis=1)
-    # Write to file
+    kept_columns = [
+        "pokemon_name",
+        "classification",
+        "pokemon_height",
+        "pokemon_weight",
+        "primary_type",
+        "secondary_type",
+        "health_stat",
+        "attack_stat",
+        "defense_stat",
+        "special_attack_stat",
+        "special_defense_stat",
+        "speed_stat",
+        "base_stat_total",
+    ]
+    df = df[kept_columns]
+
     df.to_csv("pokemon_modified.csv", index=False)
 
 
