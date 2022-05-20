979d070d04a481bd4c05a1d5d6214f822509be7d
Fisher Sun <fisher521.fs@gmail.com>
2022-05-19 22:09:04 -0400

    Use enums in models.py

--- pokesummary/models.py
+++ pokesummary/models.py
@@ -2,8 +2,31 @@ import csv
 from collections import UserDict
 from dataclasses import dataclass
 from importlib import resources
+from enum import Enum
 
 from pokesummary import data
+
+
+class PokemonType(Enum):
+    NORMAL = "Normal"
+    FIRE = "Fire"
+    WATER = "Water"
+    ELECTRIC = "Electric"
+    GRASS = "Grass"
+    ICE = "Ice"
+    FIGHTING = "Fighting"
+    POISON = "Poison"
+    GROUND = "Ground"
+    FLYING = "Flying"
+    PSYCHIC = "Psychic"
+    BUG = "Bug"
+    ROCK = "Rock"
+    GHOST = "Ghost"
+    DRAGON = "Dragon"
+    DARK = "Dark"
+    STEEL = "Steel"
+    FAIRY = "Fairy"
+    NO_TYPE = ""
 
 
 @dataclass(frozen=True)
@@ -23,8 +46,8 @@ class Pokemon:
     height: float
     weight: float
 
-    primary_type: str
-    secondary_type: str
+    primary_type: PokemonType
+    secondary_type: PokemonType
 
     base_stats: BaseStats
 
@@ -45,8 +68,8 @@ class PokemonDict(UserDict):
                     classification=csv_row["classification"],
                     height=float(csv_row["pokemon_height"]),
                     weight=float(csv_row["pokemon_weight"]),
-                    primary_type=csv_row["primary_type"],
-                    secondary_type=csv_row["secondary_type"],
+                    primary_type=PokemonType(csv_row["primary_type"]),
+                    secondary_type=PokemonType(csv_row["secondary_type"]),
                     base_stats=BaseStats(
                         hp=int(csv_row["health_stat"]),
                         attack=int(csv_row["attack_stat"]),
