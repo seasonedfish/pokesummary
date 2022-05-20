e0a57acfe2704e124571a35be1ca610a023fa51f
Fisher Sun <fisher521.fs@gmail.com>
2022-05-19 23:01:08 -0400

    Use dictionary comprehension

--- pokesummary/displaying.py
+++ pokesummary/displaying.py
@@ -26,24 +26,8 @@ multiplier_strings = {
 
 TypeDefenses = TypedDict(
     "TypeDefenses", {
-        PokemonType.NORMAL: float,
-        PokemonType.FIRE: float,
-        PokemonType.WATER: float,
-        PokemonType.ELECTRIC: float,
-        PokemonType.GRASS: float,
-        PokemonType.ICE: float,
-        PokemonType.FIGHTING: float,
-        PokemonType.POISON: float,
-        PokemonType.GROUND: float,
-        PokemonType.FLYING: float,
-        PokemonType.PSYCHIC: float,
-        PokemonType.BUG: float,
-        PokemonType.ROCK: float,
-        PokemonType.GHOST: float,
-        PokemonType.DRAGON: float,
-        PokemonType.DARK: float,
-        PokemonType.STEEL: float,
-        PokemonType.FAIRY: float
+        pokemon_type: float
+        for pokemon_type in PokemonType
     }
 )
 
